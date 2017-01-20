20da0316176f3eab89d19984a67f7df83a4f65df
James Kirkby <jkirkby@protonmai.ch>
2017-01-20 23:46:53 +0800

    make sure command uses crud create to store new entity

--- src/Console/Commands/CreateRoleCommand.php
+++ src/Console/Commands/CreateRoleCommand.php
@@ -35,6 +35,6 @@ class CreateRoleCommand extends Command
         $role = app()
             ->make('em')
             ->getRepository('\ApiArchitect\Auth\Entities\Role')
-            ->store($roleEntity);
+            ->create($roleEntity);
     }
 }
